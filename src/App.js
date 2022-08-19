edcb549adb5d054a25846acd70194cbcc77871f6
Daniel Cruz Calani <danny@Daniels-MacBook-Pro.local>
2022-08-19 00:11:54 -0400

    add cart and modal modules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
     return (
         <React.Fragment>
-            <Header></Header>
+            <Cart/>
+            <Header/>
             <main>
-                <Meals />
+                <Meals/>
             </main>
         </React.Fragment>
     );
